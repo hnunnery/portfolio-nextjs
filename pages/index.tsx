208e84e0d4598725579d14d9508c39180fd8bb21
Hunter Nunnery <hunter.nunnery@yahoo.com>
2023-03-04 19:09:53 -0500

    navbar updates

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Navbar from '../components/Navbar'
-
+import Head from "next/head";
+import Image from "next/image";
+import Navbar from "../components/Navbar";
 
 export default function Home() {
   return (
@@ -12,9 +11,8 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      
+
       <Navbar />
-  
     </div>
-  )
+  );
 }
