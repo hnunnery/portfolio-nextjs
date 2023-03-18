3e786b212bfdd2be453ec05b3400275dbb3c2e42
Hunter Nunnery <hunter.nunnery@yahoo.com>
2023-03-18 12:20:07 -0400

    Projects Component Creation

---
+++ components/Projects.jsx
@@ -0,0 +1,142 @@
+import Image from "next/image";
+
+const projects = [
+  {
+    name: "Bingeworthy - Rate Television Shows",
+    description:
+      "Authenticated users can create, read, update, and delete ratings for shows",
+    imageUrl:
+      "https://res.cloudinary.com/missionwebdev/image/upload/c_fit,h_1216/v1679155832/BingeWorthy/Screenshot_2023-03-18_at_12.02.51_PM.png",
+    badgeOne: "/../public/assets/skills/react.png",
+    badgeTwo: "/../public/assets/skills/nextjs.png",
+    badgeThree: "/../public/assets/skills/tailwind.png",
+    badgeFour: "/../public/assets/skills/firebase.png",
+  },
+  {
+    name: "Stealth Entry CyberSecurity Solutions",
+    description:
+      "Created this website for a customer using VueJS and the NuxtJS framework",
+    imageUrl:
+      "https://res.cloudinary.com/missionwebdev/image/upload/v1551139322/StealthEntry/main.jpg",
+    badgeOne: "/../public/assets/skills/react.png",
+    badgeTwo: "/../public/assets/skills/nextjs.png",
+    badgeThree: "/../public/assets/skills/tailwind.png",
+    badgeFour: "/../public/assets/skills/firebase.png",
+  },
+  {
+    name: "Veterans of War Aid Foundation",
+    description:
+      "Created this website as a volunteer developer for a non-profit organization",
+    imageUrl:
+      "https://res.cloudinary.com/missionwebdev/image/upload/v1553647520/VOWAID/vowaid-meta-card.jpg",
+    badgeOne: "/../public/assets/skills/react.png",
+    badgeTwo: "/../public/assets/skills/nextjs.png",
+    badgeThree: "/../public/assets/skills/tailwind.png",
+    badgeFour: "/../public/assets/skills/firebase.png",
+  },
+];
+
+export default function Projects() {
+  return (
+    <div className="bg-white py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl lg:mx-0">
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+            Featured Projects
+          </h2>
+          <p className="mt-6 text-lg leading-8 text-gray-600">
+            Some of my favorite projects that showcase the technologies,
+            frameworks, and skills I use in my full staack web applications.
+          </p>
+        </div>
+        <ul
+          role="list"
+          className="mx-auto mt-20 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3"
+        >
+          {projects.map((project) => (
+            <li key={project.name}>
+              <Image
+                className="aspect-[3/2] w-full rounded-2xl object-cover"
+                src={project.imageUrl}
+                alt=""
+                height="300"
+                width="400"
+              />
+              <h3 className="mt-6 text-lg font-semibold leading-8 tracking-tight text-gray-900">
+                {project.name}
+              </h3>
+              <p className="text-base leading-7 text-gray-600">
+                {project.description}
+              </p>
+              <ul role="list" className="mt-6 flex gap-x-6">
+                <li>
+                  <Image
+                    className="h-8 w-auto"
+                    src="/../public/assets/skills/html.png"
+                    alt=""
+                    height="100"
+                    width="100"
+                  />
+                </li>
+                <li>
+                  <Image
+                    className="h-8 w-auto"
+                    src="/../public/assets/skills/css.png"
+                    alt=""
+                    height="100"
+                    width="100"
+                  />
+                </li>
+                <li>
+                  <Image
+                    className="h-8 w-auto"
+                    src="/../public/assets/skills/javascript.png"
+                    alt=""
+                    height="100"
+                    width="100"
+                  />
+                </li>
+                <li>
+                  <Image
+                    className="h-8 w-auto"
+                    src={project.badgeOne}
+                    alt=""
+                    height="100"
+                    width="100"
+                  />
+                </li>
+                <li>
+                  <Image
+                    className="h-8 w-auto"
+                    src={project.badgeTwo}
+                    alt=""
+                    height="100"
+                    width="100"
+                  />
+                </li>
+                <li>
+                  <Image
+                    className="h-8 w-auto"
+                    src={project.badgeThree}
+                    alt=""
+                    height="100"
+                    width="100"
+                  />
+                </li>
+                <li>
+                  <Image
+                    className="h-8 w-auto"
+                    src={project.badgeFour}
+                    alt=""
+                    height="100"
+                    width="100"
+                  />
+                </li>
+              </ul>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
